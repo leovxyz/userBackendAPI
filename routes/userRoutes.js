0f4640131c45de9defb1d715cec429973f4ab0f4
Leo <sw.frenzy568@passinbox.com>
2024-09-08 21:40:55 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,11 @@
 const express = require('express');
+const { registerUser, loginUser, currentUser } = require('../controllers/userController');
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-    res.json({message: "Register the user"})
-});
+router.post("/register", registerUser);
 
-router.post("/login", (req, res) => {
-    res.json({message: "Login  user"})
-});
+router.post("/login", loginUser);
 
-router.get("/current", (req, res) => {
-    res.json({message: "Current user information"})
-});
+router.get("/current", currentUser);
 
 module.exports = router;
