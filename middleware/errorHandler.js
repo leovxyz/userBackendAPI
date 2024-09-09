3e2b9575f6fd7a93572c57ed820a062d727f2c8d
Leo <sw.frenzy568@passinbox.com>
2024-09-09 20:24:45 +0200

    updated reference

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,4 @@
-const {constants} = require("../constants/constants");
+const {constants} = require("../constants/constants.js");
 const errorHandler =  (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
@@ -8,37 +8,37 @@ const errorHandler =  (err, req, res, next) => {
                 message: err.message, 
                 stackTrace: err.stack
             });
-            break; // Added break to prevent fall-through
+            break;
         case constants.NOT_FOUND:
             res.json({
                 title:"Not Found",
                 message: err.message, 
                 stackTrace: err.stack
             });
-            break; // Added break to prevent fall-through
+            break;
         case constants.UNAUTHORIZED:
             res.json({
                 title:"Unauthorized",
                 message: err.message, 
                 stackTrace: err.stack
             });
-            break; // Added break to prevent fall-through
+            break;
         case constants.FORBIDDEN:
             res.json({
                 title:"Forbidden",
                 message: err.message, 
                 stackTrace: err.stack
             });
-            break; // Added break to prevent fall-through
+            break; 
         case constants.SERVER_ERROR:
             res.json({
                 title:"Server Error",
                 message: err.message, 
                 stackTrace: err.stack
             });
-            break; // Added break to prevent fall-through
+            break;
         default:
-            console.log("No error, all good!");
+            console.log("");
             break;
     }
 };
